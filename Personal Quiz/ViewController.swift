ac90168e662ce6a27c26888494bd6cfa97543f27
BertranMark <37067020+BertranMark@users.noreply.github.com>
2018-09-06 20:35:59 +0300

    Initial Commit

---
+++ Personal Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Personal Quiz
+//
+//  Created by Bertran on 06.09.2018.
+//  Copyright © 2018 Bertran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
